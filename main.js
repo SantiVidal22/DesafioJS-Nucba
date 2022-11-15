6530711044e6ad5be391c809e604ece27be7e384
Santi-Vidal <m0n5xhx@gmail.com>
2022-11-15 01:00:19 -0300

    Working

--- main.js
+++ main.js
@@ -3,37 +3,37 @@ const Pizzas = [
     {
         id: 1,
         nombre: "napolitana",
-        ingredientes: ["jamon","queso","tomate","oregano"],
+        ingredientes: ["queso mozzarella", "anchoas", "orégano", "alcaparras"],
         precio: 750,
     },
     {
         id: 2,
-        nombre: "margarita",
-        ingredientes: ["jamon","queso","tomate","oregano"],
+        nombre: "cuatro estaciones",
+        ingredientes: ["aceitunas negras", "alcachofas", "jamon", "champigñones", "queso mozzarella"],
         precio: 899,
     },
     {
         id: 3,
         nombre: "primavera",
-        ingredientes: ["jamon","queso","tomate","oregano"],
+        ingredientes: ["queso mozzarella", "tomates cherry","oregano", "rúcula", "prosciutto", "queso parmesano"],
         precio: 1350,
     },
     {
         id: 4,
         nombre: "margarita",
-        ingredientes: ["jamon","queso","tomate","oregano"],
+        ingredientes: ["queso mozzarella", "albahaca fresca"],
         precio: 2400,
     },
     {
         id: 5,
         nombre: "cuatro quesos",
-        ingredientes: ["jamon","queso","tomate","oregano"],
+        ingredientes: ["queso azul", "queso gorgonzola", "queso cremoso", "queso parmesano", "oregano"],
         precio: 2100,
     },
     {
         id: 6,
-        nombre: "margarita",
-        ingredientes: ["jamon","queso","tomate","oregano"],
+        nombre: "hawaiana",
+        ingredientes: ["queso mozzarella", "piña", "doble jamon"],
         precio: 700,
     },
 ];
@@ -46,4 +46,30 @@ console.log(idImpares);
 
 idImpares.forEach( (pizza) => {
     console.log(`La pizzas ${pizza.nombre} no tienen promo compras una te llevas 2`);
+});
+
+const Economizar = (precio) => {
+    return Pizzas.some ( (pizza) => {
+        return pizza.precio > precio;
+    })
+        ? console.log(`No hay pizzas con precio menor a $${precio}`)
+        : console.log(`Hay pizzas con precio menor a $${precio}`);
+}
+
+Economizar(600);
+
+const PrecioNombre = Pizzas.filter ((pizzas) => {
+    return pizzas.nombre && pizzas.precio
+})
+
+PrecioNombre.forEach ( (pizzas) => {
+    console.log (`Tenemos pizza ${pizzas.nombre} y su precio es $${pizzas.precio}`)
+});
+
+const PizzaIngredientes = Pizzas.filter ((pizzas) => {
+    return pizzas.nombre && pizzas.ingredientes
+})
+
+PizzaIngredientes.forEach ( (pizzas) => {
+    console.log (`Pizza ${pizzas.nombre} sus ingredientes son: ${pizzas.ingredientes}`)
 });
